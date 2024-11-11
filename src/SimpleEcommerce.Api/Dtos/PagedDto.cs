d9f6891b37908876121a3ecaa13b05118f3de5cf
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-11-12 00:02:30 +0200

    feat(catalog) : Add QuerableExtensions paging result method

---
+++ src/SimpleEcommerce.Api/Dtos/PagedDto.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleEcommerce.Api.Dtos
+{
+    public class PagedDto<T>
+    {
+        public List<T> Data { get; set; }
+        public int Skip { get; set; }
+        public int Length { get; set; }
+        public long TotalCount { get; set; }
+
+        public PagedDto()
+        {
+
+        }
+        public PagedDto(List<T> data, long count, int skip, int length)
+        {
+            Data = data;
+            TotalCount = count;
+            Skip = skip;
+            Length = length;
+
+        }
+    }
+}
---
+++ src/SimpleEcommerce.Api/Extensions/QuerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Ardalis.GuardClauses;
+using Microsoft.EntityFrameworkCore;
+using SimpleEcommerce.Api.Dtos;
+using System;
+
+namespace SimpleEcommerce.Api.Extensions
+{
+    public static class QuerableExtensions
+    {
+        public static async Task<PagedDto<T>> ToPaged<T>(this IQueryable<T> query, int skip, int lenght, CancellationToken cancellationToken = default)
+        {
+            Guard.Against.Null(query, nameof(query));
+            Guard.Against.Negative(skip, nameof(skip));
+            Guard.Against.NegativeOrZero(lenght, nameof(lenght));
+
+            var count = await query.CountAsync();
+
+            var items = await query
+                .Skip(skip)
+                .Take(lenght)
+                .ToListAsync(cancellationToken);
+
+            return new PagedDto<T>(items, count, skip, lenght);
+
+        }
+    }
+}
