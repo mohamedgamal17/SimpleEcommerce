6aaf47086c5200700b33cdc722e742dd88e6fc04
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-11-16 01:50:05 +0200

    feat(cart) : Add cart domain

---
+++ src/SimpleEcommerce.Api/Domain/Cart/Basket.cs
@@ -0,0 +1,61 @@
+﻿using SimpleEcommerce.Api.Exceptions;
+
+namespace SimpleEcommerce.Api.Domain.Cart
+{
+    public class Basket : Entity
+    {
+        public string UserId { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+
+        public Basket()
+        {
+            
+        }
+
+        public Basket(string userId)
+        {
+            UserId = userId;
+        }
+
+
+        public void AddProduct(int productId , int quantity)
+        {
+            var item = Items.SingleOrDefault(x => x.ProductId == productId);
+
+            if(item == null)
+            {
+                item = new BasketItem(Id, quantity);
+
+                Items.Add(item);
+            }
+
+            item.Quantity += quantity;
+
+            if(item.Quantity <= 0)
+            {
+                Items.Remove(item);
+            }
+        }
+
+        public void RemoveItem(int productId)
+        {
+            var item = Items.SingleOrDefault(x => x.ProductId == productId);
+            
+            if(item == null)
+            {
+                throw new EntityNotFoundException(typeof(BasketItem), productId);
+            }
+
+            Items.Remove(item);
+        }
+
+        public void Migrate(Basket basket)
+        {
+            basket.Items.ForEach(item =>
+            {
+                AddProduct(item.ProductId, item.Quantity);
+            });
+        }
+
+    }
+}
---
+++ src/SimpleEcommerce.Api/Domain/Cart/BasketItem.cs
@@ -0,0 +1,29 @@
+﻿using SimpleEcommerce.Api.Domain.Catalog;
+
+namespace SimpleEcommerce.Api.Domain.Cart
+{
+    public class BasketItem : Entity
+    {
+        public int BasketId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public Product Product { get; set; }
+
+        public BasketItem()
+        {
+            
+        }
+
+        public BasketItem(int basketId, int productId)
+        {
+            BasketId = basketId;
+            ProductId = productId;
+        }
+        public BasketItem(int basketId , int productId , int quantity)
+        {
+            BasketId = basketId;
+            ProductId = productId;
+            Quantity = quantity;
+        }
+    }
+}
