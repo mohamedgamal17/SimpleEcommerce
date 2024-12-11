b263ee65aaccc36fb90447157fc0d5758a90f287
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-12-11 19:39:12 +0200

    refactor : Add new constructor overload to support string ids Add new constructor overload in EntityNotFoundException class to support string ids

--- src/SimpleEcommerce.Api/Exceptions/EntityNotFoundException.cs
+++ src/SimpleEcommerce.Api/Exceptions/EntityNotFoundException.cs
@@ -13,5 +13,12 @@
         {
 
         }
+
+        public EntityNotFoundException(Type entity, string id) : base(
+            $"there is no such {entity.Name} entity with id : {id}"
+            )
+        {
+
+        }
     }
 }
