df6497002be8aab05cfa885ca9529bd7707d7cfb
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-12-15 21:30:05 +0200

    feat(medias) : Add PictureDto with automapper profile

---
+++ src/SimpleEcommerce.Api/Dtos/Media/MediaProfileMapping.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using SimpleEcommerce.Api.Domain.Media;
+using SimpleEcommerce.Api.Services;
+
+namespace SimpleEcommerce.Api.Dtos.Media
+{
+    public class MediaProfileMapping : Profile
+    {
+        public MediaProfileMapping()
+        {
+            CreateMap<Picture, PictureDto>()
+                .ForMember(x => x.Url, opt => opt.MapFrom<MediaUrlValueResolver>());
+        }
+    }
+
+
+    public class MediaUrlValueResolver : IValueResolver<Picture, PictureDto, string>
+    {
+        private readonly S3StorageService _s3StoragetService;
+        public MediaUrlValueResolver(S3StorageService s3StoragetService)
+        {
+            _s3StoragetService = s3StoragetService;
+        }
+        public  string Resolve(Picture source, PictureDto destination, string destMember, ResolutionContext context)
+        {
+            return _s3StoragetService.GeneratePublicUrl(source.S3Key); 
+        }
+    }
+}
---
+++ src/SimpleEcommerce.Api/Dtos/Media/PictureDto.cs
@@ -0,0 +1,12 @@
+﻿using SimpleEcommerce.Api.Domain.Media;
+
+namespace SimpleEcommerce.Api.Dtos.Media
+{
+    public class PictureDto : EntityDto
+    {
+        public string MimeType { get; set; }
+        public string AltAttribute { get; set; }
+        public string Url { get; set; }
+        public PictureType PictureType { get; set; }
+    }
+}
