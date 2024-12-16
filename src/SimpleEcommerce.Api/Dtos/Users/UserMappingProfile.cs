fd46acf8fb5932ae2710ed0139666f4b165dff53
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-12-16 19:28:38 +0200

    fix : Add missing autmapper profiles

---
+++ src/SimpleEcommerce.Api/Dtos/Users/UserMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SimpleEcommerce.Api.Domain.Users;
+
+namespace SimpleEcommerce.Api.Dtos.Users
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<User, UserDto>()
+                .ForMember(x => x.Addresses, opt => opt.MapFrom(c => c.Addresses));
+
+            CreateMap<Address, AddressDto>();
+          
+        }
+    }
+}
