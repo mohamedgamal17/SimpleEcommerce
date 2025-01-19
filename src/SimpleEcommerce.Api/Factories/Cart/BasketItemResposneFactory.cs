6ea79e92248fe7140c402e80d2288834b09b25a6
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-19 17:15:36 +0200

    feat : Add cart resposnes factory classes

---
+++ src/SimpleEcommerce.Api/Factories/Cart/BasketItemResposneFactory.cs
@@ -0,0 +1,29 @@
+﻿using SimpleEcommerce.Api.Domain.Cart;
+using SimpleEcommerce.Api.Dtos.Cart;
+using SimpleEcommerce.Api.Factories.Catalog;
+
+namespace SimpleEcommerce.Api.Factories.Cart
+{
+    public class BasketItemResposneFactory : ResponseFactory<BasketItem, BasketItemDto>
+    {
+        private readonly ProductResponseFactory _productResposnseFactory;
+
+        public BasketItemResposneFactory(ProductResponseFactory productResposnseFactory)
+        {
+            _productResposnseFactory = productResposnseFactory;
+        }
+
+        public override async Task<BasketItemDto> PrepareDto(BasketItem data)
+        {
+            var dto = new BasketItemDto
+            {
+                Id = data.Id,
+                ProductId = data.ProductId,
+                Quantity = data.Quantity,
+                Product = await _productResposnseFactory.PrepareDto(data.Product)
+            };
+
+            return dto;
+        }
+    }
+}
---
+++ src/SimpleEcommerce.Api/Factories/Cart/BasketResposneFactory.cs
@@ -0,0 +1,27 @@
+﻿using SimpleEcommerce.Api.Domain.Cart;
+using SimpleEcommerce.Api.Dtos.Cart;
+
+namespace SimpleEcommerce.Api.Factories.Cart
+{
+    public class BasketResposneFactory : ResponseFactory<Basket, BasketDto>
+    {
+        private readonly BasketItemResposneFactory _basketItemResposneFactory;
+
+        public BasketResposneFactory(BasketItemResposneFactory basketItemResposneFactory)
+        {
+            _basketItemResposneFactory = basketItemResposneFactory;
+        }
+
+        public override async Task<BasketDto> PrepareDto(Basket data)
+        {
+            var dto = new BasketDto
+            {
+                Id = data.Id,
+                UserId = data.UserId,
+                Items = await _basketItemResposneFactory.PrepareListDto(data.Items)
+            };
+
+            return dto;
+        }
+    }
+}
