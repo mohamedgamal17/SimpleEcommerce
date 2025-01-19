62f31f974ce3e77c129496ef14ce95e1f8cc5875
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-19 17:01:45 +0200

    feat : Add base ResponseFactory class

---
+++ src/SimpleEcommerce.Api/Factories/IResponseFactory.cs
@@ -0,0 +1,11 @@
+﻿using SimpleEcommerce.Api.Dtos;
+
+namespace SimpleEcommerce.Api.Factories
+{
+    public interface IResponseFactory<TEntity,TDto>
+    {
+        Task<PagedDto<TDto>> PreparePagedDto(PagedDto<TEntity> data);
+        Task<List<TDto>> PrepareListDto(List<TEntity> data);
+        Task<TDto> PrepareDto(TEntity data);
+    }
+}
---
+++ src/SimpleEcommerce.Api/Factories/ResponseFactory.cs
@@ -0,0 +1,29 @@
+﻿using SimpleEcommerce.Api.Dtos;
+
+namespace SimpleEcommerce.Api.Factories
+{
+    public abstract class ResponseFactory<TEntity, TDto> : IResponseFactory<TEntity, TDto>
+    {
+        public async Task<PagedDto<TDto>> PreparePagedDto(PagedDto<TEntity> data)
+        {
+            var paged = new PagedDto<TDto>
+            {
+                Data = await PrepareListDto(data.Data),
+                Skip = data.Skip,
+                Length = data.Length,
+                TotalCount = data.TotalCount
+            };
+
+            return paged;
+        }
+
+        public async Task<List<TDto>> PrepareListDto(List<TEntity> data)
+        {
+            var tasks = data.Select(PrepareDto);
+
+            return (await Task.WhenAll(tasks)).ToList();
+        }
+
+        public abstract Task<TDto> PrepareDto(TEntity data);
+    }
+}
