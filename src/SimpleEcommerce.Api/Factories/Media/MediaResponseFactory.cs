2dbdbfaf2ba6e83ff3c221fbb72aac8cccac642a
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-19 17:02:11 +0200

    feat : Add media resposne factory class

---
+++ src/SimpleEcommerce.Api/Factories/Media/MediaResponseFactory.cs
@@ -0,0 +1,30 @@
+﻿using SimpleEcommerce.Api.Domain.Media;
+using SimpleEcommerce.Api.Dtos.Media;
+using SimpleEcommerce.Api.Services;
+
+namespace SimpleEcommerce.Api.Factories.Media
+{
+    public class MediaResponseFactory : ResponseFactory<Picture, PictureDto>
+    {
+        private readonly IS3StorageService _s3StorageService;
+
+        public MediaResponseFactory(IS3StorageService s3StorageService)
+        {
+            _s3StorageService = s3StorageService;
+        }
+
+        public override Task<PictureDto> PrepareDto(Picture data)
+        {
+            var dto = new PictureDto
+            {
+                Id = data.Id,
+                MimeType = data.MimeType,
+                PictureType = data.PictureType,
+                AltAttribute = data.AltAttribute,
+                Url = _s3StorageService.GeneratePublicUrl(data.S3Key)
+            };
+
+            return Task.FromResult(dto);
+        }
+    }
+}
