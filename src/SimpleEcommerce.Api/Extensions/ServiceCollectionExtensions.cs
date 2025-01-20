c1d887018b064f1473a41db1e101d69651a802a6
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-20 18:14:34 +0200

    feat : Add Services dependency injection registration

--- src/SimpleEcommerce.Api/Extensions/ServiceCollectionExtensions.cs
+++ src/SimpleEcommerce.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using SimpleEcommerce.Api.Factories;
+using SimpleEcommerce.Api.Services;
 using System.Reflection;
 
 namespace SimpleEcommerce.Api.Extensions
@@ -14,6 +15,27 @@ namespace SimpleEcommerce.Api.Extensions
             foreach (var type in factoriesTypes)
             {
                 services.AddTransient(type);
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicaitonService(this IServiceCollection services , Assembly assembly)
+        {
+            var servicesTypes = assembly.GetTypes()
+                .Where(x => !x.IsAbstract && x.GetInterfaces().Any(c => c == typeof(IApplicationService)))
+                .ToList();
+
+            foreach (var serviceType in servicesTypes)
+            {
+                var serviceInterfaceType = serviceType.GetInterfaces().Where(x => x != typeof(IApplicationService)).FirstOrDefault();
+
+                if(serviceInterfaceType != null)
+                {
+                    services.AddTransient(serviceInterfaceType, serviceType);
+                }
+
+                services.AddTransient(serviceType);
             }
 
             return services;
