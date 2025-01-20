b37b338755875edf38d896a48aed2e3e18b121cd
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-20 17:18:12 +0200

    refactor : Add Identity user resposne factory

---
+++ src/SimpleEcommerce.Api/Factories/Identity/IdentityUserResponseFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SimpleEcommerce.Api.Dtos.Identity;
+
+namespace SimpleEcommerce.Api.Factories.Identity
+{
+    public class IdentityUserResponseFactory : ResponseFactory<IdentityUser, IdentityUserDto>
+    {
+        public override Task<IdentityUserDto> PrepareDto(IdentityUser data)
+        {
+            var dto = new IdentityUserDto
+            {
+                Id = data.Id,
+                UserName = data.UserName,
+                PhoneNumber = data.PhoneNumber,
+                PhoneNumberConfirmed = data.PhoneNumberConfirmed,
+                Email = data.Email,
+                EmailConfirmed = data.EmailConfirmed,
+                TwoFactorEnabled = data.TwoFactorEnabled
+            };
+
+            return Task.FromResult(dto);
+        }
+    }
+}
