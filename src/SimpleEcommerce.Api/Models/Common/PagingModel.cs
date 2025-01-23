8cce2f750da0fc34e1a32aa473b01c369ab60e75
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-23 16:26:00 +0200

    feat(data-paging) : add query validator for PagingModel to avoid EntityFramework exceptions

--- src/SimpleEcommerce.Api/Models/Common/PagingModel.cs
+++ src/SimpleEcommerce.Api/Models/Common/PagingModel.cs
@@ -1,8 +1,30 @@
-﻿namespace SimpleEcommerce.Api.Models.Common
+﻿using FluentValidation;
+
+namespace SimpleEcommerce.Api.Models.Common
 {
     public class PagingModel
     {
         public int Skip { get; set; }
         public int Limit { get; set; }
+
+        public PagingModel()
+        {
+            Skip = 0;
+            Limit = 10;
+        }
+    }
+
+    public class PagingModelValidator : AbstractValidator<PagingModel>
+    {
+        public PagingModelValidator()
+        {
+            RuleFor(x => x.Skip)
+                .GreaterThanOrEqualTo(0);
+
+            RuleFor(x => x.Limit)
+                .GreaterThan(0)
+                .LessThanOrEqualTo(150);
+                
+        }
     }
 }
